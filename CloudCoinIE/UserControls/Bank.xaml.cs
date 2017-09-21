ae3a434fb5dcd568e134ef03c6f7ae79570a2881
navraj.singh <navraj.singh@infodartmail.com>
2017-09-21 23:12:30 +0530

    Updated Bank Total Formula

--- CloudCoinIE/UserControls/Bank.xaml.cs
+++ CloudCoinIE/UserControls/Bank.xaml.cs
@@ -60,10 +60,10 @@ namespace CloudCoinIE.UserControls
             setLabelText(lblValuesTotal, Convert.ToString(bankTotals[0] + frackedTotals[0] + partialTotals[0]));
 
             setLabelText(lblNotesTotal, Convert.ToString(bankTotals[1] + frackedTotals[1] + partialTotals[1]
-                + bankTotals[2] + frackedTotals[2] + partialTotals[2])
+                + bankTotals[2] + frackedTotals[2] + partialTotals[2]
                 + bankTotals[3] + frackedTotals[3] + partialTotals[3]
                 + bankTotals[4] + frackedTotals[4] + partialTotals[4]
-                + bankTotals[5] + frackedTotals[5] + partialTotals[5]);
+                + bankTotals[5] + frackedTotals[5] + partialTotals[5]));
 
 
         }// end show
