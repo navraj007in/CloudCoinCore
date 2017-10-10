661ec9508b6ccd52493ca23fed3a98e8cc964a2a
Sean Worthington <sean@worthington.net>
2017-10-10 03:27:51 -0700

    Updated it to work with multi detect.

--- Founders/Program.cs
+++ Founders/Program.cs
@@ -16,8 +16,8 @@ namespace Founders
         // public static String rootFolder = AppContext.BaseDirectory + Path.DirectorySeparatorChar;
         public static String rootFolder = AppContext.BaseDirectory;
         public static String prompt = "> ";
-        public static String[] commandsAvailable = new String[] { "echo raida", "show coins", "import", "export", "fix fracked", "show folders", "export stack files with one note each", "quit" };
-
+      public static String[] commandsAvailable = new String[] { "Echo raida", "Show CloudCoins in Bank", "Import / Pown & Deposit", "Export / Withdraw", "Fix Fracked", "Show Folders", "Export stack files with one note each", "Help", "Quit" };
+       
         //{ "echo raida", "show coins", "import", "export", "fix fracked", "show folders", "export for sales", "quit" };
         public static int timeout = 10000; // Milliseconds to wait until the request is ended. 
         public static FileUtils fileUtils = FileUtils.GetInstance(rootFolder);
@@ -63,10 +63,13 @@ namespace Founders
                     showFolders();
                     break;
                 case "fix":
-                    fix();
+                    fix(timeout);
                     break;
                 case "dump":
                     dump();
+                    break;
+                case "help":
+                    help();
                     break;
                 default:
                     break;
@@ -138,11 +141,11 @@ namespace Founders
 
         public static void printWelcome()
         {
-            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.White;
             Console.Out.WriteLine("                                                                  ");
             Console.Out.WriteLine("                   CloudCoin Founders Edition                     ");
-            Console.Out.WriteLine("                   Version: September.19.2017                      ");
+            Console.Out.WriteLine("                      Version: October.10.2017                    ");
             Console.Out.WriteLine("          Used to Authenticate, Store and Payout CloudCoins       ");
             Console.Out.WriteLine("      This Software is provided as is with all faults, defects    ");
             Console.Out.WriteLine("          and errors, and without warranty of any kind.           ");
@@ -152,17 +155,33 @@ namespace Founders
             Console.BackgroundColor = ConsoleColor.Black;
             Console.Out.Write("  Checking RAIDA");
             echoRaida();
+            RAIDA_Status.showMs();
             //Check to see if suspect files need to be imported because they failed to finish last time. 
-            String[] suspectFileNames = new DirectoryInfo(fileUtils.suspectFolder).GetFiles().Select(o => o.Name).ToArray();//Get all files in suspect folder
+            String[] suspectFileNames = new DirectoryInfo(suspectFolder).GetFiles().Select(o => o.Name).ToArray();//Get all files in suspect folder
             if (suspectFileNames.Length > 0)
             {
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.Out.WriteLine("  Finishing importing coins from last time...");//
                 Console.ForegroundColor = ConsoleColor.White;
-                detect();
+               
+                import();//temp stop while testing, change this in production
+                grade();
+
             } //end if there are files in the suspect folder that need to be imported
 
         } // End print welcome
+        
+        
+        public static void help()
+        {
+            Console.Out.WriteLine("");
+            Console.Out.WriteLine("Customer Service:");
+            Console.Out.WriteLine("Open 9am to 11pm California Time(PST).");
+            Console.Out.WriteLine("1 (530) 500 - 2646");
+            Console.Out.WriteLine("CloudCoin.HelpDesk@gmail.com(unsecure)");
+            Console.Out.WriteLine("CloudCoin.HelpDesk@Protonmail.com(secure if you get a free encrypted email account at ProtonMail.com)");
+
+        }//End Help
 
         public static bool echoRaida()
         {
@@ -233,18 +252,15 @@ namespace Founders
 
             //Output  " 12.3"
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.BackgroundColor = ConsoleColor.Blue;
             Console.Out.WriteLine("                                                                    ");
             Console.Out.WriteLine("    Total Coins in Bank:    " + string.Format("{0,8:N0}", (bankTotals[0] + frackedTotals[0] + partialTotals[0])) + "                                ");
             Console.Out.WriteLine("                                                                    ");
             Console.Out.WriteLine("                 1s         5s         25s       100s       250s    ");
             Console.Out.WriteLine("                                                                    ");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.BackgroundColor = ConsoleColor.DarkBlue;
+    
             Console.Out.WriteLine("   Perfect:   " + string.Format("{0,7}", bankTotals[1]) + "    " + string.Format("{0,7}", bankTotals[2]) + "    " + string.Format("{0,7}", bankTotals[3]) + "    " + string.Format("{0,7}", bankTotals[4]) + "    " + string.Format("{0,7}", bankTotals[5]) + "   ");
             Console.Out.WriteLine("                                                                    ");
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.BackgroundColor = ConsoleColor.DarkBlue;
             // Console.Out.WriteLine("   Partial:   " + string.Format("{0,7}", partialTotals[1]) + "    " + string.Format("{0,7}", partialTotals[2]) + "    " + string.Format("{0,7}", partialTotals[3]) + "    " + string.Format("{0,7}", partialTotals[4]) + "    " + string.Format("{0,7}", partialTotals[5]) + "   ");
             // Console.Out.WriteLine("                                                                    ");
             Console.Out.WriteLine("   Fracked:   " + string.Format("{0,7}", frackedTotals[1]) + "    " + string.Format("{0,7}", frackedTotals[2]) + "    " + string.Format("{0,7}", frackedTotals[3]) + "    " + string.Format("{0,7}", frackedTotals[4]) + "    " + string.Format("{0,7}", frackedTotals[5]) + "   ");
@@ -267,6 +283,7 @@ namespace Founders
             Console.Out.WriteLine(" Directory:   "  + fileUtils.directoryFolder);
             Console.Out.WriteLine(" Counterfeits:"  + fileUtils.counterfeitFolder);
             Console.Out.WriteLine(" Export:      "  + fileUtils.exportFolder);
+            Console.Out.WriteLine(" Lost:        " + fileUtils.lostFolder);
         } // end show folders
 
         public static void import()
@@ -316,64 +333,22 @@ namespace Founders
                 Console.Out.WriteLine("  No coins in import folder.");// "No coins in import folder.");
                 Console.ForegroundColor = ConsoleColor.White;
             }
-            else
+             else
             {
-                detect();
+                DateTime before = DateTime.Now;
+                DateTime after;
+                TimeSpan ts = new TimeSpan();
+                multi_detect();
+                after = DateTime.Now;
+                ts = after.Subtract(before);//end the timer
+                grade();
+                Console.Out.WriteLine("Time in ms to multi detect pown " + ts.TotalMilliseconds);
+                RAIDA_Status.showMultiMs();
+                showCoins();
             }//end if coins to import
         }   // end import
 
-        public static void partialImport()
-        {
-
-            //Check RAIDA Status
-            int totalRAIDABad = 0;
-            for (int i = 0; i < 25; i++)
-            {
-                if (RAIDA_Status.failsEcho[i])
-                {
-                    totalRAIDABad += 1;
-                }
-            }
-            if (totalRAIDABad > 8)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Out.WriteLine("You do not have enough RAIDA to perform an import operation.");
-                Console.Out.WriteLine("Check to make sure your internet is working.");
-                Console.Out.WriteLine("Make sure no routers at your work are blocking access to the RAIDA.");
-                Console.Out.WriteLine("Try to Echo RAIDA and see if the status has changed.");
-                Console.ForegroundColor = ConsoleColor.White;
-                return;
-            }
-
-            //CHECK TO SEE IF THERE ARE UN DETECTED COINS IN THE SUSPECT FOLDER
-            String[] suspectFileNames = new DirectoryInfo(fileUtils.suspectFolder).GetFiles().Select(o => o.Name).ToArray();//Get all files in suspect folder
-            if (suspectFileNames.Length > 0)
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.Out.WriteLine("  Finishing importing coins from last time...");//
-                Console.ForegroundColor = ConsoleColor.White;
-                detect();
-                Console.Out.WriteLine("  Now looking in import folder for new coins...");// "Now looking in import folder for new coins...");
-            } //end if there are files in the suspect folder that need to be imported
-
-
-            Console.Out.WriteLine("");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Out.WriteLine("  Loading all CloudCoins in your import folder: ");// "Loading all CloudCoins in your import folder: " );
-            Console.Out.WriteLine(fileUtils.importFolder);
-            Console.ForegroundColor = ConsoleColor.White;
-            Importer importer = new Importer(fileUtils);
-            if (!importer.importAll())//Moves all CloudCoins from the Import folder into the Suspect folder. 
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Out.WriteLine("  No coins in import folder.");// "No coins in import folder.");
-                Console.ForegroundColor = ConsoleColor.White;
-            }
-            else
-            {
-                partialDetect();
-            }//end if coins to import
-        }   // end import
+     
 
 
         public static void detect()
@@ -399,10 +374,10 @@ namespace Founders
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
             Console.Out.WriteLine("");
-            Console.Out.WriteLine("  Partially Detecting Authentication of Suspect Coins");// "Detecting Authentication of Suspect Coins");
-            Detector detector = new Detector(fileUtils, timeout);
+            Console.Out.WriteLine("  Detecting Authentication of Suspect Coins");// "Detecting Authentication of Suspect Coins");
+            Detector detector = new Detector(fileUtils);
             int[] detectionResults = { 0 };//detector.partialDetectAll();
-            Console.Out.WriteLine("  Total imported to partial: " + detectionResults[0]);//"Total imported to bank: "
+            Console.Out.WriteLine("  Total imported to bank: " + detectionResults[0]);//"Total imported to bank: "
             Console.Out.WriteLine("  Total imported to fracked: " + detectionResults[2]);//"Total imported to fracked: "
                                                                                          // And the bank and the fractured for total
             Console.Out.WriteLine("  Total Counterfeit: " + detectionResults[1]);//"Total Counterfeit: "
@@ -491,9 +466,8 @@ namespace Founders
             Banker bank = new Banker(fileUtils);
             int[] bankTotals = bank.countCoins(fileUtils.bankFolder);
             int[] frackedTotals = bank.countCoins(fileUtils.frackedFolder);
-            int[] partialTotals = bank.countCoins(fileUtils.partialFolder);
             Console.Out.WriteLine("  Your Bank Inventory:");
-            int grandTotal = (bankTotals[0] + frackedTotals[0] + partialTotals[0]);
+            int grandTotal = (bankTotals[0] + frackedTotals[0] );
             showCoins();
             // state how many 1, 5, 25, 100 and 250
             int exp_1 = 0;
@@ -502,8 +476,8 @@ namespace Founders
             int exp_100 = 0;
             int exp_250 = 0;
             //Warn if too many coins
-            Console.WriteLine(bankTotals[1] + frackedTotals[1] + bankTotals[2] + frackedTotals[2] + bankTotals[3] + frackedTotals[3] + bankTotals[4] + frackedTotals[4] + bankTotals[5] + frackedTotals[5] + partialTotals[1] + partialTotals[2] + partialTotals[3] + partialTotals[4] + partialTotals[5]);
-            if (((bankTotals[1] + frackedTotals[1]) + (bankTotals[2] + frackedTotals[2]) + (bankTotals[3] + frackedTotals[3]) + (bankTotals[4] + frackedTotals[4]) + (bankTotals[5] + frackedTotals[5]) + partialTotals[1] + partialTotals[2] + partialTotals[3] + partialTotals[4] + partialTotals[5]) > 1000)
+            Console.WriteLine(bankTotals[1] + frackedTotals[1] + bankTotals[2] + frackedTotals[2] + bankTotals[3] + frackedTotals[3] + bankTotals[4] + frackedTotals[4] + bankTotals[5] + frackedTotals[5] );
+            if (((bankTotals[1] + frackedTotals[1]) + (bankTotals[2] + frackedTotals[2]) + (bankTotals[3] + frackedTotals[3]) + (bankTotals[4] + frackedTotals[4]) + (bankTotals[5] + frackedTotals[5])) > 1000)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.Out.WriteLine("Warning: You have more than 1000 Notes in your bank. Stack files should not have more than 1000 Notes in them.");
@@ -517,35 +491,35 @@ namespace Founders
             if ((bankTotals[1] + frackedTotals[1]) > 0)
             {
                 Console.Out.WriteLine("  How many 1s do you want to export?");
-                exp_1 = reader.readInt(0, (bankTotals[1] + frackedTotals[1] + partialTotals[1]));
+                exp_1 = reader.readInt(0, (bankTotals[1] + frackedTotals[1] )));
             }
 
             // if 1s not zero 
             if ((bankTotals[2] + frackedTotals[2]) > 0)
             {
                 Console.Out.WriteLine("  How many 5s do you want to export?");
-                exp_5 = reader.readInt(0, (bankTotals[2] + frackedTotals[2] + partialTotals[2]));
+                exp_5 = reader.readInt(0, (bankTotals[2] + frackedTotals[2] ));
             }
 
             // if 1s not zero 
             if ((bankTotals[3] + frackedTotals[3] > 0))
             {
                 Console.Out.WriteLine("  How many 25s do you want to export?");
-                exp_25 = reader.readInt(0, (bankTotals[3] + frackedTotals[3] + partialTotals[3]));
+                exp_25 = reader.readInt(0, (bankTotals[3] + frackedTotals[3] ));
             }
 
             // if 1s not zero 
             if ((bankTotals[4] + frackedTotals[4]) > 0)
             {
                 Console.Out.WriteLine("  How many 100s do you want to export?");
-                exp_100 = reader.readInt(0, (bankTotals[4] + frackedTotals[4] + partialTotals[4]));
+                exp_100 = reader.readInt(0, (bankTotals[4] + frackedTotals[4] ));
             }
 
             // if 1s not zero 
             if ((bankTotals[5] + frackedTotals[5]) > 0)
             {
                 Console.Out.WriteLine("  How many 250s do you want to export?");
-                exp_250 = reader.readInt(0, (bankTotals[5] + frackedTotals[5] + partialTotals[5]));
+                exp_250 = reader.readInt(0, (bankTotals[5] + frackedTotals[5] ));
             }
 
             // if 1s not zero 
